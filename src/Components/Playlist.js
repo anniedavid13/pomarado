a6edd68ed59a32575b8b7904677dd6cb6b59dfa8
Annie David <E20CSE068@bennett.edu.in>
2023-07-16 23:11:43 +0530

    final code pt2

--- src/Components/Playlist.js
+++ src/Components/Playlist.js
@@ -2,19 +2,21 @@ import React from 'react';
 
 const PlaylistEmbed = () => {
   return (
-    <div
-      className="fixed bottom-0 left-0 rounded-lg p-2"
-      style={{ borderRadius: '12px', width: '400px', height: '200px' }}
-    >
-      <iframe
-        src="https://open.spotify.com/embed/playlist/1MuDVTTNqRmEUV1dzyfMnB?utm_source=generator"
-        width="100%"
-        height="100%"
-        frameBorder="0"
-        allowFullScreen=""
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-        loading="lazy"
-      ></iframe>
+    <div className="fixed bottom-0 left-0 rounded-lg p-2">
+      <div
+        style={{ borderRadius: '12px', width: '400px', height: '200px' }}
+      >
+        <iframe
+          src="https://open.spotify.com/embed/playlist/0ffnLxCftwLzmXDO7DJEXc?utm_source=generator&theme=0"
+          width="100%"
+          height="100%"
+          frameBorder="0"
+          allowFullScreen=""
+          allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+          loading="lazy"
+          title="Playlist Embed"
+        ></iframe>
+      </div>
     </div>
   );
 };
